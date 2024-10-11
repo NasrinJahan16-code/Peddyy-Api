90f235db39113a2b814eb0e2a7a61c722c4f541d
NasrinJahan16-code <nasrinjh1696@gmail.com>
2024-10-10 21:49:42 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                primary: '#0E7A81',
+                secondary: '#131313'
+            },
+            fontFamily: {
+                lato: ['Lato', 'Arial', 'SansSerif'],
+            }
+        }
+    }
+}
